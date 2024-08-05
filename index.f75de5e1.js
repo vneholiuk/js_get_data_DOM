9c8a0a8e51a0ff670f4a7f0fa770ec78f11453d3
Vlados <vnegoluk@gmail.com>
2024-08-05 13:09:19 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,14 @@
+"use strict";
+const populationElement = document.getElementsByClassName("population");
+const averagePopulationElement = document.getElementsByClassName("average-population");
+const totalPopulationElement = document.getElementsByClassName("total-population");
+const totalPopulation = [
+    ...populationElement
+].reduce((prew, el)=>prew + +el.innerHTML.replaceAll(",", ""), 0);
+const avrgPopulation = totalPopulation / [
+    ...populationElement
+].length;
+averagePopulationElement[0].textContent = avrgPopulation.toLocaleString("en-US");
+totalPopulationElement[0].textContent = totalPopulation.toLocaleString("en-US");
+
+//# sourceMappingURL=index.f75de5e1.js.map
